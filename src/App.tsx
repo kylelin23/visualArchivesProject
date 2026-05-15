3a531dbd08ab9e8d48a2fd53a1f5021d84814524
kylelin23 <klin78@calpoly.edu>
2026-05-15 12:48:53 -0700

    add starting template

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Treemap</h1>
+      <div className="box">Placeholder</div>
+    </div>
+  );
+}
